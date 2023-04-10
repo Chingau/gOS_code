8090d224ed435c34406518e2cda2f47490e7fc38
chingax <3220983574@qq.com>
2023-04-10 09:01:05 -0700

    突破512字节(软盘)

--- oskernel/boot/boot.asm
+++ oskernel/boot/boot.asm
@@ -1,5 +1,8 @@
 [ORG  0x7c00]
 
+[SECTION .data]
+SETUP_MAIN_ADDR equ 0x500                   ;内核加载器在内存中的位置
+
 [SECTION .text]
 [BITS 16]
 global _start
@@ -8,18 +11,26 @@ _start:
     mov ax, 3
     int 0x10
 
-    mov     ax, 0
-    mov     ss, ax
-    mov     ds, ax
-    mov     es, ax
-    mov     fs, ax
-    mov     gs, ax
-    mov     si, ax
+    ;读软盘
+    mov ch, 0                   ;0柱面
+    mov dh, 0                   ;0磁头
+    mov cl, 2                   ;第2扇区
+    mov bx, SETUP_MAIN_ADDR     ;数据往哪读
+    mov ah, 0x02                ;读操作
+    mov al, 1                   ;连续读几个扇区
+    mov dl, 0                   ;驱动器编号
+    int 0x13
 
-    mov     si, msg
-    call    print
+    mov si, jmp_to_setup
+    call print
+    xchg bx, bx
 
-    jmp     $
+    ;jmp SETUP_MAIN_ADDR         ;移交控制权
+
+    ;正常情况下不会执行到下面3行代码
+    mov si, read_floppy_error
+    call print
+    jmp $
 
 ; 如何调用
 ; mov     si, msg   ; 1 传入字符串
@@ -34,14 +45,16 @@ print:
     cmp al, 0
     jz .done
     int 0x10
-
     inc si
     jmp .loop
 .done:
     ret
 
-msg:
-    db "hello, world", 10, 13, 0
+read_floppy_error:
+    db "read floppy error!", 10, 13, 0      ;read floppy error!\r\n0
+
+jmp_to_setup:
+    db "jump to setup...", 10, 13, 0        ;jump to setup...\r\n0
 
 ;clear zero
 times 510 - ($ - $$) db 0
